1a367996fa6c01336e1bce0d90d7a70824368074
daredev0504 <61061540+daredev0504@users.noreply.github.com>
2020-05-02 13:16:45 +0100

    header done

---
+++ app.js
@@ -0,0 +1,14 @@
+let headerVar=document.getElementsByClassName('header-name');
+headerVar.textContent='ghgh';
+
+
+let mobileOpen=()=> {
+    let openNav=document.getElementById('mobileO');
+    openNav.style.width='100%';
+    openNav.style.opacity='90%';
+}
+let mobileClose=()=> {
+    let closeNav=document.getElementById('mobileO');
+    closeNav.style.width='0';
+  
+}
